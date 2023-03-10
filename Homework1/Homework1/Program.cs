659e77d71d2af45cb914308cb9802996bd0248a3
krstevski.damjan@gmail.com <krstevski.damjan@gmail.com>
2023-03-10 12:21:22 +0100

    Homework 1

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,37 @@
+﻿#region Task1
+
+Console.WriteLine("    *  ");
+
+Console.WriteLine("   *** ");
+
+Console.WriteLine("  ***** ");
+
+Console.WriteLine(" *******");
+
+Console.WriteLine("*********");
+
+Console.ReadLine();
+
+#endregion
+
+#region Task2
+
+Console.WriteLine("   *  ");
+
+Console.WriteLine("   * ");
+
+Console.WriteLine(" * * * ");
+
+Console.WriteLine("*  *  *");
+
+Console.WriteLine("   *");
+
+Console.WriteLine("   *");
+
+Console.WriteLine(" *   *");
+
+Console.WriteLine("*     *");
+
+
+#endregion
+
